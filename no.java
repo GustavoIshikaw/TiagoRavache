919021b81963c692d924fcb80b01e75a982e1970
GustavoIshikaw <gustavo.ishikaw@gmail.com>
2025-05-15 21:59:29 -0300

    Add files via upload

---
+++ no.java
@@ -0,0 +1,14 @@
+class no {
+    string valor;
+    no esquerda, direita
+
+    public no(string valor){
+        this.valor = direita = null;
+
+    }
+}
+
+public class arvore {
+
+    no raiz,
+}
